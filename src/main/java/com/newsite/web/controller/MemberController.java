67095939918fe8a9e2ae81694a83dfff2e2a3c4e
torahoo <legokbs@gmail.com>
2023-03-26 14:40:24 +0900

    practice to make own blog site

--- src/main/java/com/newsite/web/controller/MemberController.java
+++ src/main/java/com/newsite/web/controller/MemberController.java
@@ -5,9 +5,13 @@ import com.newsite.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.annotation.PostConstruct;
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 @RequestMapping("/controller/members")
@@ -16,10 +20,17 @@ public class MemberController {
 
         private final MemberRepository memberRepository;
 
-    //테스트용 데이터
-    @PostConstruct
-    public void init () {
-        memberRepository.save(new Member("user1@naver.com", "user1", "1234", 20));
-        memberRepository.save(new Member("user2@naver.com", "user2", "1234", 30));
-    }
+        @GetMapping
+        public String membersList (Model model) {
+            List<Member> members = memberRepository.findAll();
+            model.addAttribute("members" , members);
+            return "/member/members-list";
+        }
+
+        //테스트용 데이터
+        @PostConstruct
+        public void init () {
+            memberRepository.save(new Member("user1@naver.com", "user1", "1234", 20));
+            memberRepository.save(new Member("user2@naver.com", "user2", "1234", 30));
+        }
 }
