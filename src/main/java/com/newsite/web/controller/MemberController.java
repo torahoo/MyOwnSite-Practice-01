25783b187c9573c2bc15eaac954cea1e06d5b91e
torahoo <legokbs@gmail.com>
2023-03-26 14:57:49 +0900

    practice to make own blog site

--- src/main/java/com/newsite/web/controller/MemberController.java
+++ src/main/java/com/newsite/web/controller/MemberController.java
@@ -14,23 +14,29 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Controller
+@Slf4j
 @RequestMapping("/controller/members")
 @RequiredArgsConstructor
 public class MemberController {
 
-        private final MemberRepository memberRepository;
+    private final MemberRepository memberRepository;
 
-        @GetMapping
-        public String membersList (Model model) {
-            List<Member> members = memberRepository.findAll();
-            model.addAttribute("members" , members);
-            return "/member/members-list";
-        }
 
-        //테스트용 데이터
-        @PostConstruct
-        public void init () {
-            memberRepository.save(new Member("user1@naver.com", "user1", "1234", 20));
-            memberRepository.save(new Member("user2@naver.com", "user2", "1234", 30));
-        }
+    @GetMapping
+    public String members (Model model) {
+        List<Member> members = memberRepository.findAll();
+        model.addAttribute("members" , members);
+        log.info("members={}", members);
+        return "/member/members-list";
+    }
+
+    @GetMapping("/add")
+    public String
+
+    //테스트용 데이터
+    @PostConstruct
+    public void init () {
+        memberRepository.save(new Member("user1@naver.com", "user1", "1234", 20));
+        memberRepository.save(new Member("user2@naver.com", "user2", "1234", 30));
+    }
 }
