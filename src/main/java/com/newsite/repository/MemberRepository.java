f9f581f81d0ba8ce1bde081d863cb6ab0703e2db
torahoo <legokbs@gmail.com>
2023-12-13 15:12:20 +0900

    practice to make own blog site Git practice 2 fix MemberRepository

--- src/main/java/com/newsite/repository/MemberRepository.java
+++ src/main/java/com/newsite/repository/MemberRepository.java
@@ -26,22 +26,24 @@ public class MemberRepository {
         }
 
         public void update (long memberId, Member updateParam) {
-            Member findMember = findById(memberId);
+            Member findMember = findById(memberId).orElseThrow(() -> new NoSuchElementException("No such memberId : " + memberId));
             findMember.setMemberEmail(updateParam.getMemberEmail());
             findMember.setMemberNickName(updateParam.getMemberNickName());
             findMember.setMemberAge(updateParam.getMemberAge());
         }
 
         public void delete (long memberId) {
-            Member findMember = findById(memberId);
+            Member findMember = findById(memberId).orElseThrow(() -> new NoSuchElementException("No memeber found id matches: " + memberId));
             store.remove(memberId);
         }
 
-        public Optional<Member> findByName(String name) {
-            return store.values().stream()
-                    .filter(member -> member.getName().equals(name))
-                    .findAny();
-        }
+        // 멤버 name을 필드값으로 지정한 적이 없는데 왜 이 메서드가 필요?
+        // 나중에 해결하기
+//        public Optional<Member> findByName(String name) {
+//            return store.values().stream()
+//                    .filter(member -> member.getName().equals(name))
+//                    .findAny();
+//        }
 
         public void clearStroe () {
             store.clear();
